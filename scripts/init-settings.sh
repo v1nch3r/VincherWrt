877c8304761cbfc092afd3fecc269ca30c03399b
Vincher <127501499+v1nch3r@users.noreply.github.com>
2024-03-16 16:12:02 +0700

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -18,21 +18,13 @@ phpindexfix () {
 	chmod -R 755 /www/tinyfm/assets/*
 	[ ! -d /www/tinyfm/rootfs ] && ln -s / /www/tinyfm/rootfs
 	# Autofix download index.php, index.html
-	if ! grep -q ".php=/usr/bin/php-cgi" /etc/config/uhttpd; then
-		echo -e "  helmilog : system not using php-cgi, patching php config ..."
-		logger "  helmilog : system not using php-cgi, patching php config..."
-		uci set uhttpd.main.ubus_prefix='/ubus'
-		uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi'
-		uci set uhttpd.main.index_page='cgi-bin/luci'
-		uci add_list uhttpd.main.index_page='index.html'
-		uci add_list uhttpd.main.index_page='index.php'
-		uci commit uhttpd
-		echo -e "  helmilog : patching system with php configuration done ..."
-		echo -e "  helmilog : restarting some apps ..."
-		logger "  helmilog : patching system with php configuration done..."
-		logger "  helmilog : restarting some apps..."
-		/etc/init.d/uhttpd restart
-	fi
+	uci set uhttpd.main.ubus_prefix='/ubus'
+	uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi'
+	uci set uhttpd.main.index_page='cgi-bin/luci'
+	uci add_list uhttpd.main.index_page='index.html'
+	uci add_list uhttpd.main.index_page='index.php'
+	uci commit uhttpd
+	/etc/init.d/uhttpd restart
 	[ -d /usr/lib/php8 ] && [ ! -d /usr/lib/php ] && ln -sf /usr/lib/php8 /usr/lib/php
 }
 
@@ -101,6 +93,11 @@ otherconfig () {
     
     # Fix cloudflared permissions
     chmod +x /usr/bin/cloudflared
+
+    # Delete unused menu
+    rm -r /usr/lib/lua/luci/controller/buttons.lua
+    rm -r /usr/lib/lua/luci/controller/poweroff.lua
+    rm -r /usr/lib/lua/luci/controller/rebootmodem.lua
 }
 
 phpfix
