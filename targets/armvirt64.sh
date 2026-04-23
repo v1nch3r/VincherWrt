2bdcdd0ba6ef418ac655650d415d714e406ac3c4
Isaac <isaac@v1nch3r.dev>
2026-04-23 08:08:59 -0400

    Rename all-arch-packages.txt to packages.txt
    Files now:
    - packages.txt (root) = package names for make image
    - repository/target/universal.txt = direct download URLs
    - repository/target/x86_64.txt = x86_64 specific packages
    - repository/target/armvirt64.txt = armvirt64 specific packages

--- targets/armvirt64.sh
+++ targets/armvirt64.sh
@@ -37,7 +37,7 @@ add_custom_file () {
 }
 
 build_rootfs () {
-    my_packages="$(cat "${make_path}/universal.txt")"
+    my_packages="$(cat "${make_path}/packages.txt")"
     cd ${imagebuilder_path}
     make image PROFILE="generic" PACKAGES="${my_packages}" FILES="files" || error_msg
 ## relocate rootfs
--- targets/x86_64.sh
+++ targets/x86_64.sh
@@ -36,7 +36,7 @@ add_custom_file () {
 }
 
 build_rootfs () {
-    my_packages="$(cat "${make_path}/all-arch-packages.txt")"
+    my_packages="$(cat "${make_path}/packages.txt")"
     cd ${imagebuilder_path}
     make image PROFILE="generic" PACKAGES="${my_packages}" FILES="files" || error_msg
 }
